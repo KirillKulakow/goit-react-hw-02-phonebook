27ee8cd12d425e9766f4791c7d40a40e2545b4db
Kirill <kirillkulakow31@gmail.com>
2020-04-17 20:22:29 +0300

    Updates

---
+++ precache-manifest.9f60e4c71f588a66443e08f9ad087a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29babcbeffd95ba475b341fdbcfb411c",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "bc01867eb75771f94755",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a70c22e55216faacc897",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.dca60bda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.dca60bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc01867eb75771f94755",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.cf242d97.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
