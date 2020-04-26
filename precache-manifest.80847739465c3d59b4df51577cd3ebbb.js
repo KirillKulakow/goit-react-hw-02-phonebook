d04c39d5bd032dae3c169823f192036511215505
Kirill <kirillkulakow31@gmail.com>
2020-04-26 23:05:26 +0300

    Updates

---
+++ precache-manifest.80847739465c3d59b4df51577cd3ebbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c58872a0c93d142b8be0f28d08ad6de",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c7fd48f604fc604c632e",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.97021572.chunk.css"
+  },
+  {
+    "revision": "5f12d79c7b217c67710d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.d2c3997a.chunk.css"
+  },
+  {
+    "revision": "c7fd48f604fc604c632e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c770d030.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c770d030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f12d79c7b217c67710d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.29b69cdb.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/goit-react-hw-02-phonebook/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/goit-react-hw-02-phonebook/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/goit-react-hw-02-phonebook/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/goit-react-hw-02-phonebook/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.032bdf5b3a88d2b795ced351a40b1327.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.80847739465c3d59b4df51577cd3ebbb.js"
 );
 
 self.addEventListener('message', (event) => {
